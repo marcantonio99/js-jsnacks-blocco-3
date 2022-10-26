a5dd4eeec1c634a48b26b68c472aea3e7ed66eca
marcantonio99 <marcantonio8799@gamil.com>
2022-10-26 17:30:44 +0200

    JSnack3 incomplete

---
+++ Snack3/assets/scripts/main.js
@@ -0,0 +1,11 @@
+//Fai inserire un numero che chiameremo N dall'utente.
+//Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta creato stampalo:
+let x = 0;
+let N = parseInt(prompt('Inserisci quanti array creare'));
+
+//creo un array per ogni singolo loop
+while( x < N){
+    var array = newArray();
+    let i = 0;
+
+}
